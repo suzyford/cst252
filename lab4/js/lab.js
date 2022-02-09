14500288172ecfbe33a10725c52e4aa6bf949de7
suzyford <sford@csumb.edu>
2022-02-08 16:21:05 -0800

    done

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+/** Making Homemade Pizza From Scratch.
+*Step by Step Instructions
+* @link   https://sallysbakingaddiction.com/homemade-pizza-crust-recipe/
+* @file   This files defines the MyClass class.
+* @author Sally McKinney
+* @since  January 21, 2019
+
+//Find a reliable recipe
+    //Gather all the ingredients
+       //Mix the dough ingredients together by hand or use a hand-held or stand mixer.
+           //Whisk the warm water, yeast, and granulated sugar together.
+           //Add the olive oil, salt, and flour.
+       //Knead by hand or beat the dough with your mixer for 2 minutes.
+       //Turn the dough out onto a lightly floured surface.
+           //With lightly floured hands, knead the dough for 3-4 minutes.
+       //Place dough into a greased mixing bowl.
+           //Cover tightly, and set aside to rise for about 90 minutes or overnight in the refrigerator.
+       //Punch down risen dough to release air bubbles. Divide in 2.
+       //Roll dough out into a 12-inch circle.
+           //Sprinkle flour on a clean work surface and use a rolling pin.
+       //Lightly grease baking sheet or pizza pan with nonstick spray or olive oil.
+       //Using lightly floured hands, stretch and flatten the disc onto the pan.
+       //Preheat the oven to 475°F (246°C). Allow it to heat for at least 15-20 minutes as you shape the pizza.
+       //Top with favorite pizza toppings.
+       //Bake for 15 minutes or until the bottom crust is golden brown.
---
+++ lab4/js/lab4.js
@@ -0,0 +1,14 @@
+/** Saving Your Browser's Bookmarks.
+*Step by Step Instructions
+* @link
+* @file   This files defines the MyClass class.
+* @author
+* @since
+
+//Turn on Computer.
+    //Open Your Chrome browser.
+        //In the upper right corner of the window click the menu button.
+        //Select Bookmarks>Bookmarks Manager.
+        //In the manager, click the Organize menu.
+        //Select Export bookmarks.
+            //Give it a unique name
