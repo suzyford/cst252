457937540f7c00c2e9e6654fe810275f26960b7b
suzyford <sford@csumb.edu>
2022-02-12 14:03:23 -0800

    Done

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Authors: Suzy Ford & Art Rios
+* Created: 12 February 2022
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Chrysler";
+model = "PT Cruiser";
+color = "Ruby Red";
+year = 2006;
+
+//calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
