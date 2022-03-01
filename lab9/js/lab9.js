883c934b375915936a05eaba096acc0c049882f6
suzyford <sford@csumb.edu>
2022-03-01 15:43:31 -0800

    done

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -3,3 +3,20 @@
 * Created: 02 March 2022
 * License: Public Domain
 */
+
+
+var targetEl = document.getElementById("output");
+
+var newEl = document.createElement("button");
+
+targetEl.appendChild(newEl)
+
+newEl.innerHTML = "CLICK HERE";
+
+newEl.style.backgroundColor = "limegreen";
+
+newEl.style.color = "white";
+
+newEl.style.fontSize = "30px";
+
+newEl.style.padding = "center";
