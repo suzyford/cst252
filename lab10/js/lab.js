d704f639a1c5e5e83164e209a6c56a67749a1f97
suzyford <sford@csumb.edu>
2022-03-05 11:47:34 -0800

    done

---
+++ lab10/js/lab.js
@@ -0,0 +1,17 @@
+/*
+* Authors: Suzy Ford & Art Rios
+* Created: 07 March 2022
+* License: Public Domain
+*/
+
+//button elements
+  var targetEl = document.getElementById("output");
+  var newEl = document.createElement("button");
+  targetEl.appendChild(newEl)
+  newEl.innerHTML = "CLICK HERE";
+
+//style added
+  newEl.style.backgroundColor = "limegreen";
+  newEl.style.color = "white";
+  newEl.style.fontSize = "30px";
+  newEl.style.padding = "center";
