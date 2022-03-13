93957c686c2b9721ecaf2673cb26ea5a0f0e8d87
suzyford <sford@csumb.edu>
2022-03-13 16:40:04 -0700

    done

---
+++ lab12/js/lab12.js
@@ -0,0 +1,12 @@
+/*
+* Authors: Suzy Ford & Art Rios
+* Created: 09 March 2022
+* License: Public Domain
+*/
+//thanks to this site for saving us another 2 hors! https://www.geeksforgeeks.org/how-to-set-alert-message-on-button-click-in-jquery/
+
+$(document).ready(function () {
+    $("#btn").click(function () {
+    alert("Hello! You have reached our fabulous alert message. Why? We're not sure.");
+    });
+});
